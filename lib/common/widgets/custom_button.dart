644ca438d1d5b7209387c7b7eac64072386b6860
khellud <khalidhamid993@gmail.com>
2022-11-23 08:25:24 +0300

    first commit

---
+++ lib/common/widgets/custom_button.dart
@@ -0,0 +1,26 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:flutter/material.dart';
+
+class CustomButton extends StatelessWidget {
+  final String text;
+  final VoidCallback onTap;
+  const CustomButton({
+    Key? key,
+    required this.text,
+    required this.onTap,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: onTap,
+      style: ElevatedButton.styleFrom(
+        minimumSize: const Size(
+          double.infinity,
+          50,
+        ),
+      ),
+      child: Text(text),
+    );
+  }
+}
---
+++ lib/common/widgets/custom_textfield.dart
@@ -0,0 +1,34 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:flutter/material.dart';
+
+class CustomTextField extends StatelessWidget {
+  final TextEditingController controller;
+  final String hintText;
+
+  const CustomTextField({
+    Key? key,
+    required this.controller,
+    required this.hintText,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      controller: controller,
+      decoration: InputDecoration(
+        hintText: hintText,
+        border: const OutlineInputBorder(
+          borderSide: BorderSide(
+            color: Colors.black38,
+          ),
+        ),
+        enabledBorder: const OutlineInputBorder(
+          borderSide: BorderSide(
+            color: Colors.black38,
+          ),
+        ),
+      ),
+      validator: (val) {},
+    );
+  }
+}
