9616cc220e99a65c9ae9b81439878916188dad04
khellud <khalidhamid993@gmail.com>
2022-11-23 16:00:00 +0300

    database connection

--- server/index.js
+++ server/index.js
@@ -1,1 +1,18 @@
 console.log("hello, world");
+
+const express = require("express");
+const PORT = 3000;
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.json({ name: "Khalid" });
+});
+
+app.get("/hello-world", (req, res) => {
+  res.json({ hi: "hello world!" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Connected at port ${PORT}`);
+});
